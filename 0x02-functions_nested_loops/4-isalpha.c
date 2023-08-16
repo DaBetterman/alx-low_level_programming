be2a5ab3b0eaf3fe517823027ab2117760432303
DaBetterman <1ebrahimr@gmail.com>
2023-08-16 04:33:28 -0700

    4-isalpha.c1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,14 @@
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isalpha - A function that checks for an alphabetical character.
+ * @symbol: The character to be checked.
+ *Return: 1 for alphabatical character, 0 otherwise.
  */
-#include "main.h"
-int main(void)
+
+int _isalpha(int symbol)
 {
-  if (_isalpha(c)
+if ((symbol >= 65 && symbol <= 97) || (symbol >= 97 && symbol <= 122))
+{
+return (1);
+}
+return (0);
+}
