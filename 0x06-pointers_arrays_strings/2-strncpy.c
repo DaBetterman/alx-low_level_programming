fe98008812b77a9d984738d1cc4dfcdf2e51d7a4
DaBetterman <1ebrahimr@gmail.com>
2023-08-25 01:22:32 -0700

    2-strncpy.1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,33 @@
 /**
- *
- *
- *
+ *_strncpy - A function that copies a strings.
+ *@dest: The destination that will copy the value of src.
+ *@src: The source string that will be used to copy to dest.
+ *@n: Calculates the amount of times to overwrite the dest.
+ *Return: A pointer to the resulting string
  */
-
 #include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+char *start = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0' && n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+*dest = '\0';
+while (n != 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+*dest = '\0';
+return (start);
+}
