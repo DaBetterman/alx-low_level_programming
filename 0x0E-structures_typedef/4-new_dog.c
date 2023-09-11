29cbd6dda99bab83e1f07991441496ed568e1772
DaBetterman <1ebrahimr@gmail.com>
2023-09-11 16:46:19 -0700

    4-new_dog.5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,54 +11,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 int i;
-int lenght1;
-int lenght2;
-
-struct dog *n_dog = NULL;
-
-lenght1 = 0;
-while (name[lenght1] != '\0')
-lenght1++;
-
-lenght2 = 0;
-while (owner[lenght2] != '\0')
-lenght2++;
-
+int length1 = 0;
+int length2 = 0;
+struct dog *n_dog;
+while (name[length1] != '\0')
+length1++;
+while (owner[length2] != '\0')
+length2++;
 n_dog = malloc(sizeof(struct dog));
 if (n_dog == NULL)
-{
-free(n_dog);
 return (NULL);
-}
-
-n_dog->name = malloc(lenght1 + 1);
+n_dog->name = malloc(length1 + 1);
 if (n_dog->name == NULL)
 {
-free(n_dog->name);
 free(n_dog);
 return (NULL);
 }
-
-n_dog->owner = malloc(lenght2 + 1);
+n_dog->owner = malloc(length2 + 1);
 if (n_dog->owner == NULL)
 {
+free(n_dog->name);
+free(n_dog);
+return (NULL);
+}
+for (i = 0; i < length1 && name[i] != '\0'; i++)
+n_dog->name[i] = name[i];
+n_dog->name[i] = '\0';
+for (i = 0; i < length2 && owner[i] != '\0'; i++)
+n_dog->owner[i] = owner[i];
+n_dog->owner[i] = '\0';
+n_dog->age = age;
+if (n_dog->name == NULL || n_dog->owner == NULL)
+{
+free(n_dog->name);
 free(n_dog->owner);
 free(n_dog);
 return (NULL);
 }
-
-for (i = 0; i <= lenght1; i++)
-{
-n_dog->name[i] = name[i];
-}
-
-for (i = 0; i <= lenght2; i++)
-{
-n_dog->owner[i] = owner[i];
-}
-n_dog->age = age;
-free(n_dog->name);
-free(n_dog->owner);
-free(n_dog);
 return (n_dog);
 }
