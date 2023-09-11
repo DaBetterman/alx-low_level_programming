459258f50a0d38f3fc45036f4487a1e376ef89aa
DaBetterman <1ebrahimr@gmail.com>
2023-09-11 13:28:57 -0700

    dog.h.1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Today I am learning about Structures and typedef. Another fun project.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+/**
+ * *struct dog - a new data type
+ * *@name: will create a new string variable
+ * *@age: age of the variable
+ * *@owner: will create a new string variable
+ */
+
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
