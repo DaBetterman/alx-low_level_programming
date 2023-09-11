208ede6fb3bd01d11ccf6e603ce6cf878e6f2e60
DaBetterman <1ebrahimr@gmail.com>
2023-09-11 16:26:01 -0700

    4-new_dog.4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,19 +13,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 int i;
 int lenght1;
 int lenght2;
+
 struct dog *n_dog = NULL;
+
 lenght1 = 0;
 while (name[lenght1] != '\0')
 lenght1++;
+
 lenght2 = 0;
-while (name[lenght2] != '\0')
+while (owner[lenght2] != '\0')
 lenght2++;
+
 n_dog = malloc(sizeof(struct dog));
 if (n_dog == NULL)
 {
 free(n_dog);
 return (NULL);
 }
+
 n_dog->name = malloc(lenght1 + 1);
 if (n_dog->name == NULL)
 {
@@ -33,22 +38,27 @@ free(n_dog->name);
 free(n_dog);
 return (NULL);
 }
+
 n_dog->owner = malloc(lenght2 + 1);
 if (n_dog->owner == NULL)
 {
-free(n_dog->name);
 free(n_dog->owner);
 free(n_dog);
 return (NULL);
 }
+
 for (i = 0; i <= lenght1; i++)
 {
 n_dog->name[i] = name[i];
 }
+
 for (i = 0; i <= lenght2; i++)
 {
 n_dog->owner[i] = owner[i];
 }
 n_dog->age = age;
+free(n_dog->name);
+free(n_dog->owner);
+free(n_dog);
 return (n_dog);
 }
