8307e6b2b1e8770e720ae808d32815c04e938f94
DaBetterman <1ebrahimr@gmail.com>
2023-10-30 17:30:03 +0200

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "main.h"
+
+
+/**
+ * error_file - checks if files can be opened.
+ * @location: location.
+ * @destination: destination.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int location, int destination, char *argv[])
+{
+	if (location == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (destination == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - The main function that will start and end the code.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int location, destination, fclose;
+	ssize_t count, numb;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp location destination");
+		exit(97);
+	}
+
+	location = open(argv[1], O_RDONLY);
+	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(location, destination, argv);
+
+	count = 1024;
+	while (count == 1024)
+	{
+		count = read(location, buffer, 1024);
+		if (count == -1)
+			error_file(-1, 0, argv);
+		numb = write(destination, buffer, count);
+		if (numb == -1)
+			error_file(0, -1, argv);
+	}
+
+	fclose = close(location);
+	if (fclose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", location);
+		exit(100);
+	}
+
+	fclose = close(destination);
+	if (fclose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", location);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,5 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
-void exit_with_error(int code, const char *message);
-
 
 #endif /* MAIN_H */
