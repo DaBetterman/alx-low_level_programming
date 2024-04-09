552ebd82c4ab1138e0046a6b4cac2005d9cb0bec
DaBetterman <1ebrahimr@gmail.com>
2024-04-09 13:16:50 +0200

    added task 3 and update task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,6 +19,9 @@ int linear_search(int *array, size_t size, int value)
 
 	/* size = sizeof(array) / sizeof(arr[0]); */
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i <= size; i++)
 	{
 		if (array[i] == value)
